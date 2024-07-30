35f123d00d3b2002c3fc81a51a8090b47e87d8a9
Mikhail Arepev <arepmm@gmail.com>
2024-07-30 22:35:53 +0300

    Убрал сигналы-слоты озвучки

--- ep20/src/ep20-init-other-equipment.cpp
+++ ep20/src/ep20-init-other-equipment.cpp
@@ -10,9 +10,6 @@ void EP20::initOtherEquipment(const QString &modules_dir, const QString &custom_
 
     horn = new TrainHorn();
     horn->read_config("train-horn");
-    connect(horn, &TrainHorn::soundPlay, this, &EP20::soundPlay);
-    connect(horn, &TrainHorn::soundSetVolume, this, &EP20::soundSetVolume);
-    connect(horn, &TrainHorn::soundStop, this, &EP20::soundStop);
 
     // Система подачи песка
     sand_system = new SandingSystem();
